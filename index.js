38102c596f58bdefe3c6cb678459fd215ed8fbbe
Lidya Ghilay <lidyahenok92gmail.com>
2024-03-28 12:03:33 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+// function addFive(someNumber) {
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// let result
+//   if (someNumber > 0){
+//     result = someNumber + 5
+//   }
+//   return result
+// }
+// addFive(10)
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// addFive(20)
+
+// addFive(-5)
+
+
+
+function scuberGreetingForFeet(feet){
+  
+  if(feet<= 400){
+    return "This one is on me!"
+  } 
+  else if (feet >= 400 && feet <= 2000){
+    return "That will be twenty bucks."
+  }
+  else if (feet > 2000 && feet <= 2500 ){
+    return "I will gladly take your thirty bucks."
+  }
+  else 
+    return "No can do."
+  
+  
 }
+console.log(feet(200))
+
+
+
+
+function ternaryCheckCity(destination){
+return  destination === "NYC"?"Ok, sounds good." : "No go."
+
+}
+  ternaryCheckCity("NYC")
+
+
+function switchOnCharmFromTip(Tip){
+let charm =""
+  switch (true) {
+    case Tip === "generous" :
+      charm = "Thank you so much."
+      break;
+  case Tip === "not as generous":
+    charm = "Thank you."
+    break
+  default:
+      charm =  "Bye."
+      break;
+
+  }
+  return charm
+ }
+// console.log(switchOnCharmFromTip("not as generous"))
